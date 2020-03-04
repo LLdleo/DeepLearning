5f6d1670a7aea7b6953929f880b05404f001e299
Lidian Lin <lldlld0574@gmail.com>
2020-03-03 21:01:10 -0500

    umhm

---
+++ homeworks/homework6/CNN_fashion_MNIST.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: Leo
+@contact: lldlld0574@gmail.com
+@file: CNN_fashion_mnist.py
+@time: 2020/3/1 16:30
+@desc:
+"""
---
+++ homeworks/homework6/homework6_llin.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: Leo
+@contact: lldlld0574@gmail.com
+@file: homework6_llin.py
+@time: 2020/3/1 16:36
+@desc:
+"""
